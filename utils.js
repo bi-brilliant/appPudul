e2011b741e4c8d90c74441d0aa8f42ace2de3cc9
mfebriann <muhammadfebriyan86@gmail.com>
2024-01-09 13:37:51 +0700

    refactor table transaction wallet, every swipe or click button will hit api

--- utils.js
+++ utils.js
@@ -1,269 +1,88 @@
+import {Alert} from 'react-native';
+
 // Endpoint API
 const authcode = 'fqyXqh2E47G04fm10NBDChQqURBYD965';
 export const URL_API = `https://app.xrun.run/gateway.php?authcode=${authcode}`;
 
-// * Funtion TransactionalInformation
-export const funcTransactionalInformation = async (
+const funcTransaction = async (
+  nameTransaction,
+  act,
   member,
-  currencies,
-  setTransactionalInformation,
-  Alert,
+  currency,
+  daysbefore,
 ) => {
-  totalHistory(member, currencies, setTransactionalInformation, Alert);
-  transferHistory(member, currencies, setTransactionalInformation, Alert);
-  receivedDetails(member, currencies, setTransactionalInformation, Alert);
-  transitionHistory(member, currencies, setTransactionalInformation, Alert);
+  try {
+    const request = await fetch(
+      `${URL_API}&act=${act}&startwith=0&member=${member}&currency=${currency}&daysbefore=${daysbefore}`,
+    );
+    const response = await request.json();
+    return response.data;
+  } catch (err) {
+    console.log(`Failed get ${nameTransaction}: ${err}`);
+    Alert.alert('', `Failed get ${nameTransaction}: ${err}`);
+  }
 };
 
-// ? Function Total History
-const totalHistory = async (
+export const funcTotalHistory = async (
+  nameTransaction = 'totalHistory',
+  act = 'app4200-05',
   member,
-  currencies,
-  setTransactionalInformation,
-  Alert,
+  currency,
+  daysbefore,
 ) => {
-  const fetchData = async () => {
-    try {
-      const requests = currencies.map(currency =>
-        fetch(
-          `${URL_API}&act=app4200-05&startwith=0&member=${member}&currency=${currency}&daysbefore=30`,
-          {
-            method: 'POST',
-          },
-        ),
-      );
-
-      const responses = await Promise.all(requests);
-      const results = await Promise.all(
-        responses.map(response => response.json()),
-      );
-
-      const newData = results.reduce(
-        (acc, result) => [...acc, ...result.data],
-        [],
-      );
-
-      const totalHistory = newData.map(
-        ({action, currency, datetime, time, symbol, extracode, amount}) => ({
-          action,
-          currency,
-          datetime,
-          time,
-          symbol,
-          extracode,
-          amount,
-        }),
-      );
-
-      console.log('API Total History');
-      setTransactionalInformation(prevData => ({
-        ...prevData,
-        totalHistory,
-      }));
-    } catch (error) {
-      Alert.alert(
-        'Failed',
-        "Sorry can't get data Transactional Information, try again later",
-        [
-          {
-            text: 'OK',
-            onPress: () => console.log('Failed get data Total History'),
-          },
-        ],
-      );
-      setIsLoading(false);
-    }
-  };
-
-  fetchData();
+  return await funcTransaction(
+    nameTransaction,
+    act,
+    member,
+    currency,
+    daysbefore,
+  );
 };
 
-// ? Function Transfer History
-const transferHistory = async (
+export const funcTransferHistory = async (
+  nameTransaction = 'transferHistory',
+  act = 'app4200-06',
   member,
-  currencies,
-  setTransactionalInformation,
-  Alert,
+  currency,
+  daysbefore,
 ) => {
-  const fetchData = async () => {
-    try {
-      const requests = currencies.map(currency =>
-        fetch(
-          `${URL_API}&act=app4200-06&startwith=0&member=${member}&currency=${currency}&daysbefore=30`,
-          {
-            method: 'POST',
-          },
-        ),
-      );
-
-      const responses = await Promise.all(requests);
-      const results = await Promise.all(
-        responses.map(response => response.json()),
-      );
-      console.log('API Transfer History');
-
-      const newData = results.reduce(
-        (acc, result) => [...acc, ...result.data],
-        [],
-      );
-
-      const transferHistory = newData.map(
-        ({action, currency, datetime, time, symbol, extracode, amount}) => ({
-          action,
-          currency,
-          datetime,
-          time,
-          symbol,
-          extracode,
-          amount,
-        }),
-      );
-
-      setTransactionalInformation(prevData => ({
-        ...prevData,
-        transferHistory,
-      }));
-    } catch (error) {
-      Alert.alert(
-        'Failed',
-        "Sorry can't get data Transactional Information, try again later",
-        [
-          {
-            text: 'OK',
-            onPress: () => console.log('Failed get data Total History'),
-          },
-        ],
-      );
-      setIsLoading(false);
-    }
-  };
-
-  fetchData();
+  return await funcTransaction(
+    nameTransaction,
+    act,
+    member,
+    currency,
+    daysbefore,
+  );
 };
 
-// ? Function Received details
-const receivedDetails = async (
+export const funcReceivedDetails = async (
+  nameTransaction = 'receivedDetails',
+  act = 'app4200-01',
   member,
-  currencies,
-  setTransactionalInformation,
-  Alert,
+  currency,
+  daysbefore,
 ) => {
-  const fetchData = async () => {
-    try {
-      const requests = currencies.map(currency =>
-        fetch(
-          `${URL_API}&act=app4200-01&startwith=0&member=${member}&currency=${currency}&daysbefore=30`,
-          {
-            method: 'POST',
-          },
-        ),
-      );
-
-      const responses = await Promise.all(requests);
-      const results = await Promise.all(
-        responses.map(response => response.json()),
-      );
-      console.log('API Received Details');
-
-      const newData = results.reduce(
-        (acc, result) => [...acc, ...result.data],
-        [],
-      );
-
-      const receivedDetails = newData.map(
-        ({action, currency, datetime, time, symbol, extracode, amount}) => ({
-          action,
-          currency,
-          datetime,
-          time,
-          symbol,
-          extracode,
-          amount,
-        }),
-      );
-
-      setTransactionalInformation(prevData => ({
-        ...prevData,
-        receivedDetails,
-      }));
-    } catch (error) {
-      Alert.alert(
-        'Failed',
-        "Sorry can't get data Transactional Information, try again later",
-        [
-          {
-            text: 'OK',
-            onPress: () => console.log('Failed get data Total History'),
-          },
-        ],
-      );
-      setIsLoading(false);
-    }
-  };
-
-  fetchData();
+  return await funcTransaction(
+    nameTransaction,
+    act,
+    member,
+    currency,
+    daysbefore,
+  );
 };
 
-// ? Function Transition history
-const transitionHistory = async (
+export const funcTransitionHistory = async (
+  nameTransaction = 'transitionHistory',
+  act = 'app4200-03',
   member,
-  currencies,
-  setTransactionalInformation,
-  Alert,
+  currency,
+  daysbefore,
 ) => {
-  const fetchData = async () => {
-    try {
-      const requests = currencies.map(currency =>
-        fetch(
-          `${URL_API}&act=app4200-03&startwith=0&member=${member}&currency=${currency}&daysbefore=30`,
-          {
-            method: 'POST',
-          },
-        ),
-      );
-
-      console.log('API Transition History');
-
-      const responses = await Promise.all(requests);
-      const results = await Promise.all(
-        responses.map(response => response.json()),
-      );
-
-      const newData = results.reduce(
-        (acc, result) => [...acc, ...result.data],
-        [],
-      );
-
-      const transitionHistory = newData.map(
-        ({action, currency, datetime, time, symbol, extracode, amount}) => ({
-          action,
-          currency,
-          datetime,
-          time,
-          symbol,
-          extracode,
-          amount,
-        }),
-      );
-
-      setTransactionalInformation(prevData => ({
-        ...prevData,
-        transitionHistory,
-      }));
-    } catch (error) {
-      Alert.alert(
-        'Failed',
-        "Sorry can't get data Transactional Information, try again later",
-        [
-          {
-            text: 'OK',
-            onPress: () => console.log('Failed get data Total History'),
-          },
-        ],
-      );
-      setIsLoading(false);
-    }
-  };
-
-  fetchData();
+  return await funcTransaction(
+    nameTransaction,
+    act,
+    member,
+    currency,
+    daysbefore,
+  );
 };
